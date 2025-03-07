e104470c76900571346a764c6342e85d536e566f
timothy marchant <tim.marchant@outlook.com>
2025-03-07 13:40:18 -0500

    Removed a few files and minor additions currently just getting MPLAB to communicate with github

--- SERCOM0_Methods_SPI.c
+++ SERCOM0_Methods_SPI.c
@@ -1,1 +1,14 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "definitions.h"
+//SERCOM0 SPIM
+#define SPI SERCOM0_REGS->SPIM
+//for enabling peripherals
+#define GCLKSERCOM0 GCLK_REGS->GCLK_PCHCTRL[11]
+
+#define GCLKPERDefaultMask 0x40
+void InitSPI(unsigned char baudrate){
+    GCLKSERCOM0|=GCLKPERDefaultMask;
+    SPI.SERCOM_BAUD=baudrate;
+}
--- Timer0_Methods.c
+++ Timer0_Methods.c
@@ -7,7 +7,7 @@ const unsigned int bitmasks[10]={};
 //we only will increment the timer
 void Init_TC0(const unsigned char prescaler,const unsigned char runinstandby){
     //enable timer0
-    GCLK_REGS->GCLK_GENCTRL[0]=0x00002105;
+    
     GCLK_REGS->GCLK_PCHCTRL[14]|=0b01000000;
     TC0->COUNT8.TC_CTRLBCLR=0x01;
     
